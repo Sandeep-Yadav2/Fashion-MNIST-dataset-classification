37f480f9a6c06d7f666a37f7329a6175949a9e54
SANDEEP YADAV <49448740+Sandeep-Yadav2@users.noreply.github.com>
2020-11-08 14:59:11 +0530

    Contact us added

--- fashion.py
+++ fashion.py
@@ -15,7 +15,7 @@ import Seq_model
 import pandas as pd
 import pickle
 import time
-
+#from sequential_model import history_graph
 
 import numpy as np
 from PIL import Image, ImageOps
@@ -40,7 +40,7 @@ class_names=['Tshirt/TOP','Trouser','Pullover','Dress','Coat',
 ######### -------------- Sidebarr--------------------->
 add_selectbox = st.sidebar.selectbox(
     'select the model for classification',
-    ('Sequential','CNN','About data','Pretrained Nueral network','About us')
+    ('Sequential','CNN','About data','Pretrained Neural network','Contact us')
 )
 
 
@@ -59,10 +59,10 @@ def explore_data(train_images,train_label,test_images):
     st.write('Testing Classes',len(np.unique(test_labels)))
 
 def  CNN_model_summary():
-    img=Image.open("cnn_summary.PNG")
+    img=Image.open("cnn_summary.png")
     st.image(img)
 def  Seq_model_Summary():
-    img=Image.open("Seq_summary.PNG")
+    img=Image.open("Seq_summary.png")
     st.image(img)
     
 def seq_history_graph():
@@ -73,14 +73,14 @@ def seq_history_graph():
     val_acc=history['val_accuracy']
     train_loss=history['loss']
     val_loss=history['val_loss']
-    plt.subplot(2,2,1)
-    plt.plot(train_acc,label='train acc')
-    plt.plot(val_acc,label='val acc')
+    plt.subplot(2,1,1)
+    plt.plot(train_acc,label='Training accuracy')
+    plt.plot(val_acc,label='Validation accuracy')
     plt.legend()
     plt.title('acc')
-    plt.subplot(2,2,2)
-    plt.plot(train_loss,label='train loss')
-    plt.plot(val_loss,label='val loss')
+    plt.subplot(2,1,2)
+    plt.plot(train_loss,label='Training loss')
+    plt.plot(val_loss,label='Validation loss')
     plt.legend()
     plt.title('loss')
     st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -97,13 +97,13 @@ def cnn_history_graph():
     train_loss=history['loss']
     val_loss=history['val_loss']
     plt.subplot(2,2,1)
-    plt.plot(train_acc,label='train acc')
-    plt.plot(val_acc,label='val acc')
+    plt.plot(train_acc,label='Training accuracy')
+    plt.plot(val_acc,label='Validation accuracy')
     plt.legend()
     plt.title('acc')
     plt.subplot(2,2,2)
-    plt.plot(train_loss,label='train loss')
-    plt.plot(val_loss,label='val loss')
+    plt.plot(train_loss,label='Training loss')
+    plt.plot(val_loss,label='Validation loss')
     plt.legend()
     plt.title('loss')
     st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -119,6 +119,7 @@ def seq_archi():
 
 
 def about_data(cnn_model,Seq_model):
+    
     if st.button("Explore Data"):
         explore_data(train_images,train_labels,test_images)
     
@@ -127,13 +128,12 @@ def about_data(cnn_model,Seq_model):
         CNN_model_summary()
     if st.button('CNN Model Architecture'):
         cnn_archi()
-        #CNN_Model_Summary(cnn_model)
     if st.button('Seq ModelSumarry'):
         Seq_model_Summary()
         
-    if st.button('Sequential model Architecture'):
+    if st.button('Sequntial model Architecture'):
         seq_archi()
-       
+ 
     
     
     if st.button('sequential model graph'):
@@ -144,68 +144,73 @@ def about_data(cnn_model,Seq_model):
     
 if add_selectbox=='About data':
     about_data(cnn_model,Seq_model)
-# file uploder
 
-if add_selectbox=='Pretrained Nueral network':
-    st.write("working on it, updated soon!")
-if(add_selectbox=='About us'):
+if add_selectbox=='Pretrained Neural network':
+    st.info("working on it, updated soon!")
+    
+if(add_selectbox=='Contact us'):
+    image=Image.open('Sandeep Yadav.jpg')
+    image=image.resize((400,400))
+    st.image(image)
     st.write('Sandeep Yadav')
     st.write('contact:sandeep18498@gmail.com')
 
 
-file_uploader=st.file_uploader('Upload Image for Classification:')
-st.set_option('deprecation.showfileUploaderEncoding', False)
-if file_uploader is not None:
-    image=Image.open(file_uploader)
-    text_io = io.TextIOWrapper(file_uploader)
-    image=image.resize((180,180))
-    st.image(image,'Uploaded image:')
+
+# file uploder
+
+if(add_selectbox=='CNN' or add_selectbox=='Sequential'):
+    file_uploader=st.file_uploader('Upload cloth Image for Classification:')
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+    if file_uploader is not None:
+        image=Image.open(file_uploader)
+        text_io = io.TextIOWrapper(file_uploader)
+        image=image.resize((180,180))
+        st.image(image,'Uploaded image:')
         
            
-    def classify_image(image,model):
+        def classify_image(image,model):
+            st.write("classifying......")
+            img = ImageOps.grayscale(image)
         
-        st.write("classifying......")
-        img = ImageOps.grayscale(image)
-        
-        img=img.resize((28,28))
-        if(add_selectbox=='Sequential'):
-            img=np.expand_dims(img,0)
-        else:
-            img=np.expand_dims(img,0)
-            img=np.expand_dims(img,3)
-        img=(img/255.0)
+            img=img.resize((28,28))
+            if(add_selectbox=='Sequential'):
+                img=np.expand_dims(img,0)
+            else:
+                img=np.expand_dims(img,0)
+                img=np.expand_dims(img,3)
+            img=(img/255.0)
                   
-        img=1-img
+            img=1-img
         
         
-        pred=model.predict(img)
+            pred=model.predict(img)
         
-        st.write("The Predicted image is:",class_names[np.argmax(pred)])
-        st.write('Prediction probability :{:.2f}%'.format(np.max(pred)*100))
-    st.write('Click for classify the image')
-    if st.button('Classify Image'):
-        if(add_selectbox=='Sequential'):
-            st.write("You are choosen Image classification with Sequential Model")
-            classify_image(image,seq_model)
-            with st.spinner():
-                
-                time.sleep(2)
-               
-                st.success('This Image successfully classified!')
+            st.write("The Predicted image is:",class_names[np.argmax(pred)])
+            st.write('Prediction probability :{:.2f}%'.format(np.max(pred)*100))
+        st.write('Click for classify the image')
+        if st.button('Classify Image'):
+            if(add_selectbox=='Sequential'):
+                st.write("You are choosen Image classification with Sequential Model")
+                classify_image(image,seq_model)
+                st.success('This Image successufully classified!')
+                with st.spinner('Wait for it...'):
+                    time.sleep(2)
+                    st.success('Done!')
+                    st.balloons()
                 st.balloons()
-        if(add_selectbox=='CNN'):
-            st.write("You are choosen Image classification with CNN Model")
-            classify_image(image,cnn_model)
- 
-            with st.spinner():
-                time.sleep(2)
-                st.success('This Image successfully classified!')
-                st.balloons()
-else:
-    st.write("Please select image:")
+            if(add_selectbox=='CNN'):
+                st.write("You are choosen Image classification with CNN Model")
+                classify_image(image,cnn_model)
+                st.success('This Image successufully classified!')
+                with st.spinner('Wait for it...'):
+                    time.sleep(2)
+                    st.success('Done!')
+                    st.balloons()
+    else:
+        st.write("Please select image:")
 
         
-
 
 # In[ ]:
 
