989adc8de7c6ed121e3c64d2a655f971c350889f
SANDEEP YADAV <49448740+Sandeep-Yadav2@users.noreply.github.com>
2021-02-01 00:13:09 +0530

    Update fashion.py

--- fashion.py
+++ fashion.py
@@ -20,6 +20,19 @@ import time
 
 import numpy as np
 from PIL import Image, ImageOps
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("url_goes_here")
+    }
+   .sidebar .sidebar-content {
+        background: url("url_goes_here")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 # In[3]:
